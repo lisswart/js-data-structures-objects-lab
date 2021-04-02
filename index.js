ca82dd6d942a4d20999ef462934124257e57a1c3
lisswart <arnoudlisa@gmail.com>
2021-04-01 21:44:48 -0400

    commit tree 11b875918dc52d3d7217941c47e6ee5f08885a34

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+let driver = {address: '11 11th St'};
+function updateDriverWithKeyAndValue(driver, key, value) {
+    const copyDriver = {...driver};
+    copyDriver[key] = value;
+    return copyDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value;
+    return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+    const copyDriver = Object.assign({}, driver);
+    delete copyDriver[key];
+    return copyDriver;
+}
+
+console.log(deleteFromDriverByKey(driver, "address"));
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key];
+    return driver;
+}
+
